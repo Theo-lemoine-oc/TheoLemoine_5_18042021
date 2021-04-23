7f4e80f6e130534d7c0590533131abfd0a7305aa
Theo-lemoine-oc <76206562+Theo-lemoine-oc@users.noreply.github.com>
2021-04-23 14:38:55 +0200

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const cameraRoutes = require('./routes/camera');
+const teddyRoutes = require('./routes/teddy');
+const furnitureRoutes = require('./routes/furniture');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(express.static('images'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/api/cameras', cameraRoutes);
+app.use('/api/teddies', teddyRoutes);
+app.use('/api/furniture', furnitureRoutes);
+
+module.exports = app;
---
+++ frontend/js/index.js
@@ -0,0 +1,14 @@
+main()
+
+function main() {
+    const produits = getProduits()
+    displayProduits(produits)
+}
+
+function getProduits() {
+    return ""
+}
+
+function displayProduits() {
+    return ""
+}
