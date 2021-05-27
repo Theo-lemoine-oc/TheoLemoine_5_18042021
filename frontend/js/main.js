31fadc449238aefb7b3163195c5682d1d3aed22b
Théo <propjhh>
2021-05-27 11:56:28 +0200

    add commentary on javascript

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,25 +1,30 @@
+//Fonction principale récupère et affiche les produits sur la page
 (async function() {
     const products = await getProducts()
-    console.log(products)
     displayProducts(products)
 })()
 
+//Fonction qui permet de récupérer les produits dans l'API
 function getProducts() {
+    //On saisit l'URL de l'API, on utilise fetch
     return fetch("http://localhost:3000/api/teddies")
+        //Les promesses
         .then(function(httpBodyResponse) {
             return httpBodyResponse.json()
         })
         .then(function(products) {
             return products
         })
+        // En cas d'erreur
         .catch(function(error) {
             alert(error)
         })
 }
 
+//Fonction permettant d'afficher les produits dans le HTML
 function displayProducts(products) {
     products.forEach(function(produit) {
-        console.log(produit)
+        //On crée les éléments HTML
         let container = document.getElementById('container')
         let carte = document.createElement("div")
         let bottomCarte = document.createElement("div")
@@ -30,6 +35,7 @@ function displayProducts(products) {
         let prix = document.createElement("h4")
         let btn = document.createElement("a")
 
+        //On structure les éléments dans le HTML
         container.appendChild(carte)
         carte.appendChild(image)
         carte.appendChild(texteDiv)
@@ -39,7 +45,7 @@ function displayProducts(products) {
         bottomCarte.appendChild(prix)
         bottomCarte.appendChild(btn)
 
-
+        //On donne des attributs à nos éléments (une classe par exemple)
         carte.setAttribute('class', 'carte')
         image.setAttribute('class', 'image')
         texteDiv.setAttribute('class', 'content')
@@ -50,19 +56,30 @@ function displayProducts(products) {
         btn.setAttribute('button', 'click')
         btn.setAttribute('class', 'btn')
 
+        /*
+         * On rend notre HTML dynamique, il récupère donc les informations des produits
+         * depuis l'API pour les afficher ensuite
+         */
         titre.textContent = produit.name;
         description.textContent = produit.description;
         prix.textContent = "Prix: " + produit.price + "€";
         image.src = produit.imageUrl;
         btn.textContent = "Voir produit"
 
+        /*
+         * On dit au bouton "Voir produit" qu'il doit rediriger vers la
+         * page produit du produit séléctionné grâce à son ID
+         */
         btn.addEventListener('click', function() {
             btn.href = 'pages/produit.html?id=' + produit._id
         })
-
-        console.log(produit._id)
     })
 }
+
+
+
+
+
 
 /*
 const searchId = new URL(window.location.href).searchParams.get("id")
@@ -82,16 +99,4 @@ let id = idOurson.get(idOurson);
 console.log(idOurson);
 
 let url = fetch("http://localhost:3000/api/teddies/_id")
-*/
-
-
-
-/* 
-for (var i = 0; i < products.length; i++) {
-        var prods = document.createElement('div')
-        var title = document.createTextNode(products[i].name)
-        prods.appendChild(title)
-        var carts = document.getElementById('cards')
-        cards.insertAdjacentHTML('afterbegin', prods)
-    }
 */
